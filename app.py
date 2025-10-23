661c0ac939545ad5616b01c71783d1c9d8b246b2
MultiCloudOpsDev <rajashrigahane2018@gmail.com>
2025-10-23 15:11:50 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+import boto3
+
+# Create EC2 client
+ec2 = boto3.client('ec2', region_name='ap-south-1')  # Mumbai region
+
+# Describe all EC2 instances
+response = ec2.describe_instances()
+
+# Loop through instances and print details
+for reservation in response['Reservations']:
+    for instance in reservation['Instances']:
+        print("Instance ID:", instance['InstanceId'])
+        print("State:", instance['State']['Name'])
+        print("Type:", instance['InstanceType'])
+        print("Private IP:", instance.get('PrivateIpAddress', 'N/A'))
+        print("Public IP:", instance.get('PublicIpAddress', 'N/A'))
+        print("-" * 10)
